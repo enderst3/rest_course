22dfd3fc5d2caeeb71e0099bb7caf7403c4b2c7f
Todd Enders <toddenders@Todds-MacBook-Pro.local>
2020-05-19 16:14:27 -0700

    tutorial 1

--- tutorial_1/app.py
+++ tutorial_1/app.py
@@ -6,12 +6,37 @@ export FLASK_APP=app.py
 flask run
 """
 
-from flask import Flask
+# import libraries
+from flask import Flask, jsonify
+# start flask application
 app = Flask(__name__)
 
+# creates route localhoust '/'
 @app.route('/')
+# create a response for the request that came to '/'
 def hello_world():
+    # return string
     return "Hello World!"
+
+# create route '/hithere'
+@app.route('/hithere')
+# create a response for the request that came to '/hithere'
+def hi_there():
+    return "I just hit /hithere"
+
+# create route for '/bye'
+@app.route('/bye')
+# create a response for the request that came to '/bye'.
+def bye():
+    # can also return python 
+    retJson = {
+        'field1' : 'abc',
+        'field2' : 'def'
+    }
+    return jsonify(retJson)
+
+    
+
 
 if __name__=="__main__":
     app.run()
